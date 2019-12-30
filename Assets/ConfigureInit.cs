f15a820b52eade40a67f7b361586fb493c4aea6b
Fabian Mendez <ofmendez@gmail.com>
2019-12-30 03:37:27 -0500

    Commit previo al sdk de AWS

---
+++ Assets/ConfigureInit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ConfigureInit : MonoBehaviour {
+    public UnityEvent onPlay;
+    void Awake() {
+        onPlay.Invoke();
+    }
+
+}
---
+++ Assets/NewsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NewsController : MonoBehaviour {
+    void Start() {
+        // A correct website page.
+        StartCoroutine(GetRequest("https://www.example.com"));
+
+        // A non-existing page.
+        StartCoroutine(GetRequest("https://error.html"));
+    }
+
+    IEnumerator GetRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError) {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            } else {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+
+}
